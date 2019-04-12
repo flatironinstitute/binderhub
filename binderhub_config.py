dd7cc3f5c0fa2c9d5456ae8995b19d1ba7af2161
Dylan Simon <dylan@dylex.net>
2019-04-12 16:01:37 -0400

    Add file config layer, mounts for CuratedRepoProvider

--- binderhub_config.py
+++ binderhub_config.py
@@ -1,30 +1,56 @@
 import os
 import string
-import escapism
+import yaml
 from traitlets import Dict, Unicode
-from binderhub.repoproviders import RepoProvider, GitHubRepoProvider
+from binderhub.app import BinderHub
+from binderhub.repoproviders import RepoProvider, GitHubRepoProvider, GistRepoProvider, GitRepoProvider, GitLabRepoProvider
 
 class CuratedRepoProvider(RepoProvider):
     """Curated meta-repo provider.
 
-    This provider requires a meta-repo that contains the specifications for other repositories.
-    Specs match the name of a file in this repository.
+    This provider uses a meta-config that contains the specifications for other repositories.
+    This can be used as a level of indirection to restrict what can be launched.
     """
 
     name = Unicode('Curated')
 
-    repos = Dict(
+    config_file = Unicode(
         config=True,
-        help="""The meta configuration to use.""")
+        help="""
+        Path to the yaml/json config file.
+        {N} is expanded to the Nth component of the repo specification.""")
+
+    providers = Dict(#BinderHub.repo_providers.default_value,
+        {
+            'gh': GitHubRepoProvider,
+            'gist': GistRepoProvider,
+            'git': GitRepoProvider,
+            'gl': GitLabRepoProvider,
+        },
+        config=True,
+        help="""Repo Providers to register""")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        path = self.spec.split('/')
-        params = self.repos
-        for p in path:
+
+        safe_chars = frozenset(string.ascii_letters + string.digits + '-_/')
+        if set(self.spec) - safe_chars:
+            raise ValueError('Invalid characters in spec')
+
+        spec = [x for x in self.spec.split('/') if x]
+        path = self.config_file.format(*spec)
+        with open(path) as f:
+            params = yaml.safe_load(f)
+        for p in spec:
             params = params[p]
         self.params = params
-        self.provider = params['provider'](config = self.config, **params)
+
+        provider = self.providers[params.get('provider', 'gh')]
+        try:
+            spec = params['spec']
+        except KeyError:
+            spec = params['repo'] + '/' + params.get('branch', 'master')
+        self.provider = provider(config = self.config, spec = spec)
 
     def get_repo_url(self):
         return self.provider.get_repo_url()
@@ -36,10 +62,9 @@ class CuratedRepoProvider(RepoProvider):
         return self.provider.get_build_slug()
 
     def get_launch_options(self):
-        safe_chars = set(string.ascii_letters + string.digits)
         options = {'volumes': [], 'volume_mounts': []}
-        for mount, path in self.params.get('mounts', {}).items():
-            name = escapism.escape(mount, safe=safe_chars, escape_char='-')
+        for idx, (mount, path) in enumerate(self.params.get('mounts', {}).items(), 1):
+            name = 'mount%d'%idx
             if not mount.startswith('/'):
                 mount = '/home/jovyan/' + mount
             options['volumes'].append({'name': name, 'hostPath': {'path': path, 'type': 'Directory'}})
@@ -53,13 +78,7 @@ c.BinderHub.use_registry = True
 c.BinderHub.build_namespace = 'binder'
 c.BinderHub.template_path = 'templates'
 c.BinderHub.repo_providers = {'fi': CuratedRepoProvider}
-c.CuratedRepoProvider.repos = {
-    'triqstest': {
-        'provider': GitHubRepoProvider,
-        'spec': 'TRIQS/binder/master',
-        'mounts': {
-            'stuff': '/simons/scratch/dylan/gaea'
-        }
-    }
-}
+c.CuratedRepoProvider.config_file = '/user/{0}/.binders.yaml'
 c.DockerRegistry.token_url = ''
+c.KubeSpawner.cpu_limit = 0.5
+c.KubeSpawner.mem_limit = '1G'
