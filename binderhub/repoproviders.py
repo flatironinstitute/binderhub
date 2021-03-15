3ef555dee211c09ef1927083a63039d85717808a
Dylan Simon <dylan@dylex.net>
2021-03-15 19:16:57 -0400

    curatedrepoprovider: add spec label

--- binderhub/repoproviders.py
+++ binderhub/repoproviders.py
@@ -1050,7 +1050,7 @@ class CuratedRepoProvider(RepoProvider):
 
     def get_launch_options(self):
         options = {'volumes': [], 'volume_mounts': [],
-                'extra_labels': {'specuser': self.specuser}
+                'extra_labels': {'spec': self.spec, 'specuser': self.specuser}
             }
         options.update(self.default_options)
         for idx, (mount, path) in enumerate(self.mounts.items(), 1):
